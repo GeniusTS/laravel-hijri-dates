71c61df69c8265fd763462d540da390b94ee7f1d
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2017-05-05 15:57:16 +0300

    Add languages files

---
+++ resources/lang/en/dates.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    // array of days names
+    'days'       => ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+
+    // array of short days names
+    'short_days' => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+
+    // array of months names
+    'months'     => [
+        'Muḥarram',
+        'Ṣafar',
+        'Rabi‘ al-awwal',
+        'Rabi‘ at-akhir',
+        'Jumada al-ula',
+        'Jumada al-akhirah',
+        'Rajab',
+        'Sha‘ban',
+        'Ramadan',
+        'Shawwal',
+        'Thu al-Qa‘dah',
+        'Thu al-Hijjah',
+    ],
+
+    // array of periods
+    'periods'    => ['am', 'pm'],
+];
