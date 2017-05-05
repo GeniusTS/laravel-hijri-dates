c14d250d42e614905928c09a7ec2ffc59e383fde
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2017-05-05 16:00:05 +0300

    Add package's service provider

---
+++ src/HijriDatesServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace GeniusTS\LaravelHijri;
+
+
+use GeniusTS\HijriDate\Date;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class HijriDatesServiceProvider
+ *
+ * @package GeniusTS\LaravelHijri
+ */
+class HijriDatesServiceProvider extends ServiceProvider
+{
+
+    protected $configuration = __DIR__ . '/../resources/config/hijri-dates.php';
+
+    protected $translations = __DIR__ . '/../resources/lang';
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([$this->configuration => config_path('hijri-dates.php')], 'config');
+        $this->publishes([$this->translations => resource_path('lang/vendor/hijri-dates/')], 'translation');
+
+        $this->mergeConfigFrom($this->configuration, 'hijri-dates');
+        $this->loadTranslationsFrom($this->translations, 'hijri-dates');
+
+        $translation = Config::get('hijri-dates.translation');
+        Date::setTranslation(new $translation);
+        Date::setAdjustment(Config::get('hijri-dates.adjustment'));
+        Date::setToStringFormat(Config::get('hijri-dates.toStringFormat'));
+        Date::setDefaultNumbers(Config::get('hijri-dates.numeric_system'));
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+}
